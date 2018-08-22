11e8df93a685ef69d5fcb5483f8d766cc6878ed6
Stefan Majewsky <majewsky@gmx.net>
2018-08-22 21:38:06 +0200

    for vt6/vt6#30: implement new netstring-based message format

--- src/core/msg/tests.rs
+++ src/core/msg/tests.rs
@@ -16,220 +16,122 @@
 *
 ******************************************************************************/
 
-use std::collections::HashMap;
-use std::fs::File;
-use std::io::{BufReader, BufRead};
-
-use regex::Regex;
-
 use core::msg::*;
 
 #[test]
-fn parse_barewords() {
-    assert_eq!(Atom::parse_byte_string(b"core.set"),
-        Ok(Atom { value: String::from("core.set"), was_quoted: false }),
+fn test_message_parsing() {
+    //simple happy cases
+    expect_parses(b"{4|4:want,4:core,1:1,1:2,}",
+        ("", "want"), &[ b"core", b"1", b"2" ],
     );
-    assert_eq!(Atom::parse_byte_string(b" \tcore.set"),
-        Ok(Atom { value: String::from("core.set"), was_quoted: false }),
+    expect_parses(b"{1|9:sig.claim,}",
+        ("sig", "claim"), &[],
     );
-    assert_eq!(Atom::parse_byte_string(b" \tcore.set\t "),
-        Err(ParseError { kind: ParseErrorKind::ExpectedEOF, offset: 10 }),
+
+    //argument that is not valid UTF-8
+    expect_parses(b"{3|8:core.set,13:example.bytes,3:\xA0+\xC3,}",
+        ("core", "set"), &[ b"example.bytes", b"\xA0+\xC3" ],
     );
-    assert_eq!(Atom::parse_byte_string(b"42x"),
-        Ok(Atom { value: String::from("42x"), was_quoted: false }),
-    );
-    assert_eq!(Atom::parse_byte_string(b"?set"),
-        Err(ParseError { kind: ParseErrorKind::InvalidToken, offset: 0 }),
-    );
+
+    //lower bounds for integers (many of those are errors, but the
+    //errors occur *after* the integer parsing, so the integers were parsed
+    //correctly)
+    expect_parse_fails(b"{0|}", 3, ExpectedMessageType);
+    expect_parse_fails(b"{1|0:,}", 6, InvalidMessageType);
+    expect_parses(b"{2|4:want,0:,}", ("", "want"), &[ b"" ]);
+
+    //upper bounds for integers (the numbers are usize::max_value() - 1 for
+    //usize == u16, usize == u32 and usize == u64; so at least one of those
+    //should move the cursor backwards in the buffer when wrapping integer
+    //arithmetic is used without proper checks)
+    expect_parse_incomplete(b"{2|4:want,65535:x,}");
+    expect_parse_incomplete(b"{2|4:want,4294967295:x,}");
+    expect_parse_incomplete(b"{2|4:want,18446744073709551201:x,}");
+
+    //various UnexpectedEOF scenarios
+    expect_parse_incomplete(b"{4|4:want,4:core,1:1,1:2,");
+    expect_parse_incomplete(b"{4|4:want,4:core,1:1,1:2");
+    expect_parse_incomplete(b"{4|4:want,4:co");
+    expect_parse_incomplete(b"{4|4:want,4:");
+    expect_parse_incomplete(b"{4|4:want,4");
+    expect_parse_incomplete(b"{4|");
+    expect_parse_incomplete(b"{4");
+    expect_parse_incomplete(b"{");
+    expect_parse_fails(b"{4|4:want,4:core,1:1,}", 21, ExpectedDecimalNumber);
+
+    //unexpected characters in various situations
+    expect_parse_fails(b"{4|4:want,4:core,1:1,1:2,#", 25, ExpectedMessageCloser);
+    expect_parse_fails(b"{4|4:want,4:core,1:1,1:2#", 24, ExpectedStringCloser);
+    expect_parse_fails(b"{4|4:want,4:core,1:1,1#", 22, ExpectedStringSigil);
+    expect_parse_fails(b"{4#", 2, ExpectedListSigil);
+    expect_parse_fails(b"{#", 1, ExpectedDecimalNumber);
+    expect_parse_fails(b"#", 0, ExpectedMessageOpener);
+
+    //various other situations
+    expect_parse_fails(b"{10000000000000000000000000000", 30, DecimalNumberTooLarge);
+    expect_parse_fails(b"{01|9:sig.claim,}", 3, DecimalNumberHasLeadingZeroes);
+    expect_parse_fails(b"{1|09:sig.claim,}", 5, DecimalNumberHasLeadingZeroes);
 }
 
-#[test]
-fn parse_quoted_strings() {
-    assert_eq!(Atom::parse_byte_string(b"\"core.set\""),
-        Ok(Atom { value: String::from("core.set"), was_quoted: true }),
-    );
-    assert_eq!(Atom::parse_byte_string(b" \t\"core.set\""),
-        Ok(Atom { value: String::from("core.set"), was_quoted: true }),
-    );
-    assert_eq!(Atom::parse_byte_string(b" \t\"core.set\"\t "),
-        Err(ParseError { kind: ParseErrorKind::ExpectedEOF, offset: 12 }),
-    );
-    assert_eq!(Atom::parse_byte_string(br#""42\\x""#),
-        Ok(Atom { value: String::from("42\\x"), was_quoted: true }),
-    );
-    assert_eq!(Atom::parse_byte_string(br#""?set""#),
-        Ok(Atom { value: String::from("?set"), was_quoted: true }),
-    );
-    assert_eq!(Atom::parse_byte_string(br#""foo\nbar""#),
-        Err(ParseError { kind: ParseErrorKind::UnknownEscapeSequence, offset: 4 }),
-    );
-}
-
-//A shorthand to make the literals down below more compact.
-fn make_atom(was_quoted: bool, value: &'static str) -> Atom {
-    Atom { was_quoted: was_quoted, value: String::from(value) }
-}
-
-#[test]
-fn atom_behavior() {
-    let a1 = make_atom(false, "abc");
-    let a2 = make_atom(true, "abc");
-    assert_eq!(&a1, "abc");
-    assert_eq!(&a2, "abc");
-    assert_eq!("abc", &a1);
-    assert_eq!("abc", &a2);
-}
-
-#[test]
-fn parse_sexpressions() {
-    assert_eq!(SExpression::parse_byte_string(b"()"),
-        Ok(SExpression(vec![])),
-    );
-    assert_eq!(SExpression::parse_byte_string(br#"(aaa)"#),
-        Ok(SExpression(vec![
-            Element::Atom(make_atom(false, "aaa")),
-        ])),
-    );
-    assert_eq!(SExpression::parse_byte_string(br#"(aaa (bbb) fff)"#),
-        Ok(SExpression(vec![
-            Element::Atom(make_atom(false, "aaa")),
-            Element::SExpression(SExpression(vec![
-                Element::Atom(make_atom(false, "bbb")),
-            ])),
-            Element::Atom(make_atom(false, "fff")),
-        ])),
-    );
-    assert_eq!(SExpression::parse_byte_string(br#"(aaa (bbb "c\"\\d" eee) fff)"#),
-        Ok(SExpression(vec![
-            Element::Atom(make_atom(false, "aaa")),
-            Element::SExpression(SExpression(vec![
-                Element::Atom(make_atom(false, "bbb")),
-                Element::Atom(make_atom(true, "c\"\\d")),
-                Element::Atom(make_atom(false, "eee")),
-            ])),
-            Element::Atom(make_atom(false, "fff")),
-        ])),
-    );
-}
-
-#[test]
-fn serialize_message() {
-    let msg = Message(SExpression(vec![
-        Element::Atom(Atom::new(String::from("core1.test"))),
-        Element::Atom(Atom::new(String::from(r#"a"\"b"#))),
-    ]));
-    assert_eq!(format!("{}", msg), r#"(core1.test "a\"\\\"b")"#);
-}
-
-fn parse_sexp_get_error_msg(input: &[u8]) -> String {
-    format!("{}", SExpression::parse_byte_string(input).unwrap_err())
-}
-fn parse_message_get_error_msg(input: &[u8]) -> String {
-    format!("{}", Message::parse_byte_string(input).unwrap_err())
-}
-
-#[test]
-fn check_error_messages() {
-    assert_eq!(
-        parse_sexp_get_error_msg(b"(foo (bar"),
-        "Parse error at offset 9: unexpected EOF",
-    );
-    assert_eq!(
-        parse_sexp_get_error_msg(b"(foo \"aaa\x80bbb\")"),
-        "Parse error at offset 9: invalid UTF-8",
-    );
-    assert_eq!(
-        parse_sexp_get_error_msg(b"(foo [bar])"),
-        "Parse error at offset 5: unexpected character at start of token",
-    );
-    assert_eq!(
-        parse_sexp_get_error_msg(b"(foo \"bar\\nbaz\")"),
-        "Parse error at offset 9: unknown escape sequence",
-    );
-    assert_eq!(
-        parse_message_get_error_msg(b"(foo bar)"),
-        "Parse error at offset 0: invalid message type",
-    );
-    assert_eq!(
-        parse_message_get_error_msg(b"((want core1) core2)"),
-        "Parse error at offset 0: invalid message type",
-    );
-    assert_eq!(
-        parse_message_get_error_msg(b"()"),
-        "Parse error at offset 0: missing message type",
-    );
-    assert_eq!(
-        parse_message_get_error_msg(b"(want core1)(something else)"),
-        "Parse error at offset 12: expected EOF",
-    );
-}
-
-#[test]
-fn conformance_test_parse_sexp() {
-    let file = File::open("../conformance-tests/core/parse-sexp.txt").unwrap();
-    let mut current_input = String::from("");
-    let mut current_sexp: ParseResult<SExpression> = Ok(SExpression(Vec::new()));
-
-    let test_rx = Regex::new(r"^test\s*(.+)$").unwrap();
-    let ok_rx = Regex::new(r"^ok\s*(.+)$").unwrap();
-    let error_rx = Regex::new(r"^error\s*(\d+)\s*(.+)$").unwrap();
-
-    let error_map = {
-        let mut m = HashMap::new();
-        m.insert("expected EOF", ParseErrorKind::ExpectedEOF);
-        m.insert("invalid escape sequence", ParseErrorKind::UnknownEscapeSequence);
-        m.insert("invalid start of atom", ParseErrorKind::InvalidToken);
-        m.insert("invalid start of S-expression", ParseErrorKind::InvalidToken);
-        m.insert("invalid UTF-8", ParseErrorKind::InvalidUTF8);
-        m.insert("unexpected EOF in quoted string", ParseErrorKind::UnexpectedEOF);
-        m.insert("unexpected EOF in S-expression", ParseErrorKind::UnexpectedEOF);
-        m
-    };
-
-    for (idx, line) in BufReader::new(file).lines().enumerate() {
-        let line = line.unwrap();
-        if line.starts_with("#") {
-            continue;
-        }
-        if let Some(caps) = test_rx.captures(&line) {
-            let input = unescape_str(&caps[1]);
-            current_input = String::from_utf8_lossy(&input).into();
-            current_sexp = SExpression::parse_byte_string(&input);
-        } else if let Some(caps) = ok_rx.captures(&line) {
-            let sexp_str = String::from_utf8(unescape_str(&caps[1])).unwrap();
-            assert_eq!(
-                current_sexp.as_ref().map(serialize_sexp), Ok(sexp_str),
-                "\ninput: {:?} at line {}", current_input, idx + 1,
-            );
-        } else if let Some(caps) = error_rx.captures(&line) {
-            assert!(current_sexp.is_err(),
-                "\n input: {:?} at line {}\noutput: {:?}", current_input, idx + 1, current_sexp,
-            );
-            if let Err(ref err) = current_sexp {
-                assert_eq!(err.offset, caps[1].parse().unwrap(),
-                    "\n input: {:?} at line {}\noutput: Err({:?})", current_input, idx + 1, err,
-                );
-                assert_eq!(Some(&err.kind), error_map.get(&caps[2]),
-                    "\n input: {:?} at line {}\noutput: Err({:?})", current_input, idx + 1, err,
-                );
-            }
-        }
+fn expect_parses(input: &[u8], message_type: (&str, &str), args: &[&[u8]]) {
+    let (msg, offset) = Message::parse(input).unwrap();
+    //`input` should not contain extraneous characters
+    assert_eq!(input.len(), offset);
+    assert_eq!(msg.type_name(), message_type);
+    let mut iter = msg.arguments();
+    for expected in args {
+        assert_eq!(iter.next(), Some(*expected));
     }
+    assert_eq!(iter.next(), None);
 }
 
-fn unescape_str(input: &str) -> Vec<u8> {
-    let escape_rx = Regex::new(r"\\x([0-9A-F]{2})|(.)").unwrap();
-    let mut result = Vec::with_capacity(input.len());
-    for caps in escape_rx.captures_iter(input) {
-        if let Some(match1) = caps.get(1) {
-            result.push(u8::from_str_radix(match1.as_str(), 16).unwrap());
+fn expect_parse_fails(input: &[u8], offset: usize, kind: ParseErrorKind) {
+    let err = Message::parse(input).unwrap_err();
+    assert_eq!(err.kind, kind);
+    assert_eq!(err.offset, offset);
+}
+
+fn expect_parse_incomplete(input: &[u8]) {
+    expect_parse_fails(input, input.len(), UnexpectedEOF);
+}
+
+#[test]
+fn test_message_formatting() {
+    let mut buf = vec![0; 4096];
+    let required_size = make_example_message(&mut buf).unwrap();
+    assert_eq!(&buf[0..required_size], b"{3|4:have,4:core,3:1.0,}" as &[u8]);
+
+    //test that MessageFormatter correctly aborts when encountering too-small
+    //buffers of various sizes
+    for size in 0..(2 * required_size) {
+        let mut buf = vec![0; size];
+        let result = make_example_message(&mut buf);
+        if size < required_size {
+            assert_eq!(result, Err(BufferTooSmallError));
         } else {
-            result.extend_from_slice(caps[2].as_bytes());
+            assert_eq!(result, Ok(required_size));
         }
     }
-    result
+
+    //test a message without arguments
+    let size = MessageFormatter::format(&mut buf, "sig.claim", 0, |_| Ok(())).unwrap();
+    assert_eq!(&buf[0..size], b"{1|9:sig.claim,}" as &[u8]);
+
+    //test a message with a large number of arguments
+    let size = MessageFormatter::format(&mut buf, "foo.bar", 1000, |f| {
+        for _ in 0..1000 {
+            f.add_argument(&0)?;
+        }
+        Ok(())
+    }).unwrap();
+    //prefix "{1001|7:foo.bar;" and suffix "}" have 17 bytes in total, and each
+    //argument "1:0;" has 4 bytes
+    assert_eq!(size, 4017);
 }
 
-fn serialize_sexp(input: &SExpression) -> String {
-    format!("{}", input)
+fn make_example_message(buf: &mut [u8]) -> Result<usize, BufferTooSmallError> {
+    MessageFormatter::format(buf, "have", 2, |f| {
+        f.add_argument("core")?;
+        f.add_argument(b"1.0" as &[u8]) //TODO use ModuleVersion
+    })
 }
